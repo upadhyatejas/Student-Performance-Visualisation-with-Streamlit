860fad7cb2415df7e54660d55cea2b85117db206
Tejas Upadhya <33410261+upadhyatejas@users.noreply.github.com>
2019-12-15 22:50:57 +0530

    [added legends, 3d scatter plot and histograms]

--- StudPerfVis.py
+++ StudPerfVis.py
@@ -15,14 +15,19 @@ if st.sidebar.checkbox('click to show dataframe'):
 #create a scatter plot on test scores and colour based on gender
 st.sidebar.subheader('scatter plot')
 col2 = st.sidebar.selectbox('Which feature on y ?', df.columns[2:])
+col = st.sidebar.selectbox("which legend scheme on graph ? ", ['Bloom level', 'gender'])
 
 #create figure using plotly express
-fig = px.scatter(df, x = range(0, 1000), y = col2)
+fig = px.scatter(df, x = range(0, 1000), y = col2, color = col)
+fig0 = px.scatter_3d(df, x = df['java score'], y = df['python score'], z = df['DS score'], color = col)
 #plot
 st.plotly_chart(fig)
+st.plotly_chart(fig0)
 
+# create histogram
 st.sidebar.subheader('histogram')
 feature = st.sidebar.selectbox('which feature ? ', df.columns[2:])
-fig2 = px.histogram(df, x = feature, marginal = 'rug')
+col1 = st.sidebar.selectbox("which feature to merge,", ['gender', 'Bloom level'])
+fig2 = px.histogram(df, x = feature, marginal = 'rug', color = col1)
 st.plotly_chart(fig2)
 
